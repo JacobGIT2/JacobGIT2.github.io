f975d3dc57e0b07b8fa91859c7a71e357e8af1c3
DZC <3632942686@qq.com>
2024-06-02 20:42:15 +0800

    feature:订购页&本地储存订单

--- display.js
+++ display.js
@@ -1,6 +1,7 @@
 // 展示商品页面
 // 从后端获取商品名
 const api_url="http://127.0.0.1:5000/";
+let product;
 
 window.onload=()=>{
     let fruits=document.getElementById("fruit-display");
@@ -9,17 +10,29 @@ window.onload=()=>{
         num=1;//默认为1
     // 通过location.search获取get字符串内容
 
-    fetch(api_url+"display/?num="+num).then(
+    product=fetch(api_url+"display/?num="+num).then(
         res=>res.json()
     ).then(
         data=>{
+            let cur=localStorage.getItem(product[0]);
+            if(!cur)
+                cur=1;
+            cur=parseInt(cur);
             fruits.innerHTML+=`
-                <div class="fruit-choice">
-                    <img src="jpg/${data[0]}.jpeg" alt="">
-                    <h4 class="fruit-name">${data[1]}</h4>
-                    <p class="price">价格 <span>￥${data[2]}</span></p>
-                    <div class="but-buy">购买</div>
-                </div>`;
+            <img src="jpg/${data[0]}.jpeg" alt="">
+            <div class="descriptions">
+                <h1 class="fruit-name">${data[1]}</h1>
+                <hr>
+                <h3 class="price">价格 <span>￥${data[2]}</span></h3>
+                <input type="number" id="num" value="${cur}"><label for="num">数量</label>
+                <div onclick="return buy()" class="but-file">加入购物车</div>
+            </div>`;
         }
     );
+}
+function buy(){
+    // 加入购物车
+    let cur=document.getElementById("num").value;
+    localStorage.setItem(product[0],cur);
+    // alert(cur)
 }
