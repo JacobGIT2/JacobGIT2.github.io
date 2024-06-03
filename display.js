ebd61168b98ffd7b3236b4401f59b69e60c378e3
DZC <3632942686@qq.com>
2024-06-03 13:05:27 +0800

    feature:付款页，添加跳转到主页的链接

--- display.js
+++ display.js
@@ -1,20 +1,21 @@
 // 展示商品页面
 // 从后端获取商品名
 const api_url="http://127.0.0.1:5000/";
-let product;
+let product, num=location.search.slice(5);//商品编号
 
 window.onload=()=>{
     let fruits=document.getElementById("fruit-display");
-    let num=location.search.slice(5);//商品编号
+
     if(!num && num!==0)
         num=1;//默认为1
     // 通过location.search获取get字符串内容
 
-    product=fetch(api_url+"display/?num="+num).then(
+    fetch(api_url+"display/?num="+num).then(
         res=>res.json()
     ).then(
         data=>{
-            let cur=localStorage.getItem(product[0]);
+            product=data;
+            let cur=localStorage.getItem(num);
             if(!cur)
                 cur=1;
             cur=parseInt(cur);
@@ -25,7 +26,10 @@ window.onload=()=>{
                 <hr>
                 <h3 class="price">价格 <span>￥${data[2]}</span></h3>
                 <input type="number" id="num" value="${cur}"><label for="num">数量</label>
-                <div onclick="return buy()" class="but-file">加入购物车</div>
+                <div class="wrap-but">
+                    <div onclick="return buy();" class="but-buy">加入购物车</div>
+                    <div onclick="return remove();" class="but-clear">移除该商品</div>
+                </div>
             </div>`;
         }
     );
@@ -33,6 +37,12 @@ window.onload=()=>{
 function buy(){
     // 加入购物车
     let cur=document.getElementById("num").value;
-    localStorage.setItem(product[0],cur);
+    localStorage.setItem(num,cur);
     // alert(cur)
+}
+function remove(){
+    // 清除出购物车
+    localStorage.removeItem(num);
+    document.getElementById("num").value='1';
+    alert("已移除该商品");
 }
---
+++ payment.js
@@ -0,0 +1,43 @@
+
+const api_url="http://127.0.0.1:5000/";
+
+window.onload=()=>{
+    let total=0;
+    let orders=document.getElementById("orders");
+    let show_tot=document.getElementById("total");
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);//水果编号
+        let num = localStorage.getItem(key);//数量
+        fetch(api_url+"display/?num="+key).then(
+            res=>res.json()
+        ).then(
+            data=>{
+                let price=parseInt(data[2])*parseInt(num);
+                orders.innerHTML+=`
+                <div class="product">
+                    <img src="jpg/${data[0]}.jpeg"  height="60">
+                    <h4>${data[1]}</h4>
+                    <p>${num}个 <span>${price}HKD</span></p>
+                    <div class="but-buy" onclick="change(${key});">
+                        <p>修改</p>
+                    </div>
+                </div>`
+                total+=price;
+                show_tot.innerHTML=`${total} HKD`;
+            }
+        );
+    }
+    console.log(total);
+    
+}
+function buy(){
+    // 一定要交给后端验证！
+    localStorage.clear();
+}
+
+const display_page="display.html?num=";
+// 不能带/不然出错
+function change(num){
+    window.location.href=display_page+num;
+    // get方法访问display.html
+}
